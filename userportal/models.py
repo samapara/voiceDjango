d97decfac7610199b0cc70a0e50bf7262aca7021
srushti98 <srushtipawar12@gmail.com>
2020-04-08 01:05:28 +0530

    audios retrievable based on users on /audio_list/ and added a user field in audio model

--- userportal/models.py
+++ userportal/models.py
@@ -7,6 +7,7 @@ from .validators import validate_file_extension
 
 class AudioUploadModel(models.Model):
     title = models.CharField(max_length=200, blank=True, null=True)
+    username_ofaudiouploaded = models.CharField(max_length=200, blank=True, null=True)
     audio = models.FileField(upload_to='audios/', validators=[validate_file_extension])
 
     def __str__(self):
--- userportal/views.py
+++ userportal/views.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 from .forms import AudioUploadForm, CreateUserForm
 from .models import AudioUploadModel
@@ -55,7 +56,9 @@ def logout_view(request):
 
 @login_required(login_url='login')
 def audio_list(request):
-    audio_list = AudioUploadModel.objects.all()
+    #    audio_list = AudioUploadModel.objects.all()
+    audio_list = AudioUploadModel.objects.all().filter(username_ofaudiouploaded=request.user)
+    # audio_list = my_model.objects.all()
     context = {
         'audios': audio_list
     }
@@ -63,10 +66,19 @@ def audio_list(request):
 
 
 def indexPage(request):
+
     if request.method == 'POST':
         form = AudioUploadForm(request.POST, request.FILES)
+        if request.user.is_authenticated:
+            user = request.user
+            print(user)
         if form.is_valid():
-            form.save()
+            #print(User.get_username())
+            my_model = AudioUploadModel(username_ofaudiouploaded=request.user , title=request.POST.get('title'), audio=request.FILES['audio'])
+
+        # print(my_model.audio)
+            my_model.save()
+        # form.save()
             return redirect('/audio_list')
     else:
         form = AudioUploadForm()
@@ -74,3 +86,4 @@ def indexPage(request):
         'form': form
     }
     return render(request, 'index.html', context)
+
